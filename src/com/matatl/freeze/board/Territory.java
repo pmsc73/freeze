9d828c60ab18a567658acdd7b138104b131b0119
pmsc73 <philipmstc@gmail.com>
2015-01-25 18:17:12 -0330

    Commented on Territory class

--- src/com/matatl/freeze/board/Territory.java
+++ src/com/matatl/freeze/board/Territory.java
@@ -12,6 +12,12 @@ public class Territory {
 	private int owner;
 	private ArrayList<Integer> unitsOnFront;
 	private ArrayList<Integer> transferredUnits;
+	
+	/**
+	 * Territory
+	 * @param x,y: Coordinates of territory
+	 * Creates a new object with x and y coordinates set
+	 */
 	public Territory(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -19,6 +25,12 @@ public class Territory {
 		unitsOnFront = new ArrayList<Integer>(Game.MAX_PLAYERS);
 		transferredUnits = new ArrayList<Integer>(Game.MAX_PLAYERS);
 	}
+	/**
+	 * isAdjacent
+	 * @param other: Territory to test adjacency to
+	 * @return whether or not the target is adjacent
+	 * NOTE: this method is using HEXAGONAL base for calculations
+	 */
 	public boolean isAdjacent(Territory other) {
 		int diffX = this.x-other.x;
 		int diffY = this.y-other.y;
@@ -31,17 +43,46 @@ public class Territory {
 		}
 		return false;
 	}
+	/**
+	 * GETTERS
+	 * getColor, getOwner, getTransfers, getUnitsOnFront 
+	 */
 	public String getColor() {
 		if(owner<Game.MAX_PLAYERS)
 			return Game.PLAYER_COLORS[owner];
 		return "#000";
 	}
-	
+	public int getOwner() {
+		return owner;
+	}
+	public List<Integer> getTransfers() {
+		return transferredUnits;
+	}
+	public List<Integer> getUnitsOnFront() {
+		return unitsOnFront;
+	}
+	/**
+	 * size
+	 * @return the number of OWNER units in territory
+	 */
+	public int size() {
+		return unitsOnFront.get(owner);
+	}
+	/**
+	 * reinforce
+	 * @param count: the number of units to reinforce with
+	 * This method is to use for transferring FRIENDLY --> FRIENDLY
+	 */
 	public void reinforce(int count) {
-		Integer army = unitsOnFront.get(owner);
+		Integer army = size();
 		army+=count;
 	}
-	
+	/**
+	 * removeCasualties
+	 * @param player: which player is having casualties removed
+	 * @param casualties: how many units are being removed
+	 * Removes units owned by the given player from the territory
+	 */
 	public void removeCasualties(int player, int casualties) {
 		Integer pArmy = unitsOnFront.get(player);
 		pArmy -= casualties;
@@ -49,20 +90,37 @@ public class Territory {
 			pArmy = 0;
 		}
 	}
-	
+	/**
+	 * transferUnits
+	 * @param target: where you are moving units
+	 * @param units: how many units you are moving
+	 * Attempts to move the indicated number of units into target territory
+	 */
+	public void transferUnits(Territory target, int units) {
+		if(units > size()) {
+			// error in transfer
+			return;
+		}
+		if(target.getOwner() == this.owner) {
+			target.reinforce(units);
+			Integer u = unitsOnFront.get(owner);
+			u-=units;
+		}
+	}
+	/**
+	 * setOwner
+	 * @param player
+	 * Sets owner of territory to player
+	 */
 	public void setOwner(int player) {
 		owner = player;
 	}
-	public int getOwner() {
-		return owner;
-	}
 	
-	public List<Integer> getTransfers() {
-		return transferredUnits;
-	}
-	public List<Integer> getUnitsOnFront() {
-		return unitsOnFront;
-	}
+	/**
+	 * resolve 
+	 * This method is invoked by the battle manager
+	 * to implement the outcome of a battle on the tile
+	 */
 	public void resolveBattle() {
 		battler.resolve(this);
 	}
